df34e3eda9cfa4640d9ad14200f8b0e39d798bce
Jonathan Woster <jonathanwoster@gmail.com>
2022-03-08 16:09:08 -0600

    changes in score & cycle2 related to segment count

--- snake/game/casting/score.py
+++ snake/game/casting/score.py
@@ -15,12 +15,13 @@ class Score(Actor):
     """
     def __init__(self):
         super().__init__()
-        self._points = 0
+        # self._points = 0
         # self.add_points(0)
         cycle1 = Cycle1()  # added
         self._segment_count = len(cycle1.get_segments())  # Gets the length of the segment list
         self.update_seg_count(self._segment_count)
         # self.add_points(self._segment_count) # Sets up initial segment count
+        print(f"count at end of Score's __init__ is {self._segment_count}")  # for debugging
 
         position = Point(constants.SCORE_X, constants.SCORE_Y)  # Import location of score from Constants file
         self.set_position(position) # Set the position of the score
@@ -33,16 +34,17 @@ class Score(Actor):
         """
         self._seg_count = segment_count
         self.set_text(f"Segments Created: {self._seg_count}")  # Updated to show Segments created instead of points
+        print(f"count at end of Score's update_seg_count method is {self._seg_count}")  # for debugging
     
     
     # ideally the method below will not be needed, depending on what happens 
     # in other classes and methods as we modify the program
-    def add_points(self, points):
+    #def add_points(self, points):
         """Adds the given points to the score's total points.
         
         Args:
             points (int): The points to add.
         """
         
-        self._points += points
-        self.set_text(f"Segments Created: {self._points}")  # Updated to show Segments created instead of points
+    #   self._points += points
+    #   self.set_text(f"Segments Created: {self._points}")  # Updated to show Segments created instead of points
